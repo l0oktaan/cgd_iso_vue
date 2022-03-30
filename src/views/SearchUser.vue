8355c2c9619854f5a90fde92243004c21b442647
l0oktaan <looktaan.tc3@gmail.com>
2022-03-30 23:04:32 +0700

    650330-2304

--- src/views/SearchUser.vue
+++ src/views/SearchUser.vue
@@ -1,16 +1,86 @@
 <template>    
 <div class="container">
-    <div class="search">
+    <div class="search" v-if="!adv_toggle">
         <i class="fa fa-search"></i>
         <input type="text" class="searchTerm" placeholder="ค้นหา" 
-            id="search" v-model="keyword"@keydown.enter="search"
+            id="search" v-model="keyword" @keydown.enter="search"
         data-search>                    
         <div class="clear" data-clear @click="keyword=''"></div>              
         
     </div>
+    <div class="advsearch">
+        <div class="toggle_search" v-if="!adv_toggle">
+            <v-btn
+                elevation="2"
+                @click="adv_toggle=!adv_toggle"
+            >Advance Search</v-btn>
+        </div>
+        
+        <div class="filter" v-else>
+            <v-row>
+                <v-col cols="2"><div class="cname">ตัวเลือก :</div></v-col>
+                <v-col cols="3">
+                    <v-combobox
+                        v-model="search_select"
+                        :items="search_with"                        
+                        outlined
+                        dense
+                    ></v-combobox>
+                </v-col>
+                <v-col cols="4">
+                    <v-text-field                        
+                        placeholder="Keyword"
+                        outlined
+                        dense
+                    ></v-text-field>
+                </v-col>
+                <v-col>
+                    <v-btn
+                        elevation="2"                    
+                    ><i class="fa fa-plus"></i></v-btn>
+                </v-col>
+            </v-row>
+            <div class="toggle_search">
+                <v-btn
+                    elevation="2"
+                    :disabled="arr_adv_search.length==0"
+                >Search</v-btn>
+                <v-btn
+                    elevation="2"
+                    @click="adv_toggle=!adv_toggle"
+                >Cancle</v-btn>
+            </div>
+        </div>
+    </div>
     <div class="show">
+        
         <div class="detail" v-for="(item,index) in results" :key="index">
-
+            
+            <v-row>
+                <v-col cols="3"><div class="cname">ชื่อ - นามสกุล :</div></v-col>
+                <v-col>{{item.firstname + ' ' + item.surename}}</v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="3"><div class="cname">AccountName :</div></v-col>
+                <v-col>{{item.SamAccountName}}</v-col>
+            </v-row>
+            <v-row>
+                <v-col cols="3"><div class="cname">สังกัด :</div></v-col>
+                <v-col>{{item.company}}</v-col>
+            </v-row>
+             <v-row>
+                <v-col cols="3"><div class="cname">กลุ่มงาน :</div></v-col>
+                <v-col>{{item.department}}</v-col>
+            </v-row>
+             <v-row>
+                <v-col cols="3"><div class="cname">เปลี่ยนรหัสผ่านล่าสุด :</div></v-col>
+                <v-col>{{getThaiDateTime(item.pwdlastset)}}</v-col>
+            </v-row>
+             <v-row>
+                <v-col cols="3"><div class="cname">ใส่รหัสผ่านผิดพลาดล่าสุด :</div></v-col>
+                <v-col>{{getThaiDateTime(item.badpasswordtime)}}</v-col>
+            </v-row>
+            
         </div>
     </div>
 </div>
@@ -21,7 +91,16 @@ export default {
   data(){
     return  {
         results : null,
-        keyword: ''
+        keyword: '',
+        search_with: [
+            {id: 0,text: 'ค้นหาด้วย'},
+            {id: 1,text: 'ชื่อ-สกุล'},
+            {id: 2,text: 'Account'},
+            {id: 3,text: 'ประเภทผู้ใช้'},
+        ],
+        search_select: {id: 0,text: 'ค้นหาด้วย'},
+        adv_toggle: false,
+        arr_adv_search: []
     }
   },
   mounted(){
@@ -58,23 +137,26 @@ export default {
 </script>
 <style scoped>
 .container{
-    width: 100vh;
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    background: #f5f2f2;
+    box-sizing: border-box;
 }
 .container .search{
     
     display: flex;
     width: 400px;
-    height: 60px;		
+    height: 50px;		
     justify-content:flex-start;
     margin: 30px 0 10px;
     border: 1px solid #3d3d3d;
     border-radius: 60px;
     align-items: center;
     background: #fff;    
+    padding: 0px!important;
 
 }
 .container .search .clear{
@@ -116,7 +198,7 @@ export default {
     border: 0;
     border-right: none;
     padding: 5px;
-    height: 50px;
+    height: 35px;
     background: rgb(255, 255, 255);
     outline: none;
     color: #000000;
@@ -130,7 +212,37 @@ export default {
     flex-direction: column;
 }
 .container .show .detail{
-    width: 500px;
-    border-radius: 5px;
+    width: 100%;
+    border-radius: 10px;
+    margin: 10px;
+    padding: 10px;
+    background: #f5f2f2;
+    box-shadow:  5px 5px 10px #d4d4d4,
+            -5px -5px 10px #ffffff;
+                
+    
+}
+.cname{
+    widows: 100%;
+    text-align: right;
+    font-weight: 500;
+}
+.container .advsearch{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
+}
+.container .advsearch .toggle_search{
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    
+}
+.container .advsearch button{
+    margin: 5px;
+    
 }
 </style>
